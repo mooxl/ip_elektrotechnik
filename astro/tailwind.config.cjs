908d1f6929a73f23a483061930eaa8d57f5af595
Max Schmidt <max.schmidt@outlook.de>
2023-08-23 00:44:24 +0200

    test
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -10,6 +10,12 @@ module.exports = {
       padding: "10px",
     },
     extend: {
+      screens: {
+        1536: { max: "1536px", min: "1280px" },
+        1280: { max: "1280px", min: "1024px" },
+        1024: { max: "1024px", min: "768px" },
+        768: { max: "768px" },
+      },
       colors: {
         mint: {
           DEFAULT: "#00B1AC",
