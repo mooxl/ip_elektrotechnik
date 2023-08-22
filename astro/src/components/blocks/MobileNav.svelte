146c181e9bef405dfd9e69ec0c3eb535cf9ab531
Max Schmidt <max.schmidt@outlook.de>
2023-08-23 01:00:00 +0200

    Add toggle functionality for MobileNav

--- astro/src/components/blocks/MobileNav.svelte
+++ astro/src/components/blocks/MobileNav.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
     import type { Page } from "@/types";
+    import { slide } from "svelte/transition";
 
     interface PageWithChildren extends Page {
         children: PageWithChildren[];
     }
     export let navigation: PageWithChildren[];
+    let open = false;
     console.log(navigation)
 </script>
+
+<button on:click={()=>{open = !open}}>Hallo</button>
+{#if open}
+    <div transition:slide={{axis:"x"}} class="h-screen w-10 bg-red-400 fixed top-14 right-0"></div>
+{/if}
