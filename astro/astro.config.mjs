5f1826ca5ed333fdb58185e99c4a4ca1f7b68f97
Max Schmidt <max.schmidt@outlook.de>
2023-08-22 20:45:57 +0200

    Remove prefetch and update Navigation styling

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
@@ -19,9 +18,6 @@ export default defineConfig({
     }),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    prefetch({
-      selector: "a",
     }),
     sitemap(),
     svelte(),
