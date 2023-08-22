2144d160774044677a4effd0cd1c7ece2e52808e
Max Schmidt <max.schmidt@outlook.de>
2023-08-23 01:15:03 +0200

    Refactor MobileNav component for better mobile navigation menu

--- astro/src/components/blocks/MobileNav.svelte
+++ astro/src/components/blocks/MobileNav.svelte
@@ -10,15 +10,21 @@
     console.log(navigation)
 </script>
 
-<button on:click={()=>{open = !open}}>Hallo</button>
+<button class="space-y-1" on:click={()=>{open = !open}}>
+<div class="w-5 h-[2px] bg-mint"></div>
+<div class="w-5 h-[2px] bg-mint"></div>
+<div class="w-5 h-[2px] bg-mint"></div>
+</button>
 {#if open}
-    <div transition:slide={{axis:"x"}} class="h-screen w-auto bg-white pl-5 pr-10 space-y-5 shadow-md py-5 fixed top-14 right-0">
+    <div transition:slide={{axis:"x"}} class="h-screen w-auto overflow-hidden bg-white pl-5 pr-10 space-y-5 shadow-md py-5 fixed top-14 right-0">
     {#each navigation as item}
     <div class="flex flex-col gap-x-3">
         <a class="text-lg font-semibold" href={item.path+"/"}>{item.title}</a>
         {#each item.children as child}
-            <a href={child.path+"/"} class="text-base">{child.title}</a>
+        <a href={child.path+"/"} class="text-base">{child.title}</a>
         {/each}
+        <a class="text-lg font-semibold" href="/impressum">Impressum</a>
+        <a class="text-lg font-semibold" href="/datenschutz">Datenschutz</a>
     </div>
     {/each}
     </div>
