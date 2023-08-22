c6a6cb0211480ceb660c36c476f3cb16bb20ec77
Max Schmidt <max.schmidt@outlook.de>
2023-08-23 01:07:51 +0200

    Update MobileNav to default to open state

--- astro/src/components/blocks/MobileNav.svelte
+++ astro/src/components/blocks/MobileNav.svelte
@@ -6,11 +6,20 @@
         children: PageWithChildren[];
     }
     export let navigation: PageWithChildren[];
-    let open = false;
+    let open = true;
     console.log(navigation)
 </script>
 
 <button on:click={()=>{open = !open}}>Hallo</button>
 {#if open}
-    <div transition:slide={{axis:"x"}} class="h-screen w-10 bg-red-400 fixed top-14 right-0"></div>
+    <div transition:slide={{axis:"x"}} class="h-screen w-auto bg-white pl-5 pr-10 space-y-5 shadow-md py-5 fixed top-14 right-0">
+    {#each navigation as item}
+    <div class="flex flex-col gap-x-3">
+        <a class="text-lg font-semibold" href={item.path+"/"}>{item.title}</a>
+        {#each item.children as child}
+            <a href={child.path+"/"} class="text-base">{child.title}</a>
+        {/each}
+    </div>
+    {/each}
+    </div>
 {/if}
